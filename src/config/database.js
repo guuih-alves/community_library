65c9710fb04c0c9699b7ae1fb3715c51f28bcb93
guuih_alves <guilherme_alves_08@hotmail.com>
2025-07-17 21:50:30 -0300

    feat: create user repository insert

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import sqlite3 from 'sqlite3';
+
+const db  = new sqlite3.Database('library_db.sqlite', (err) =>{
+    if(err){
+        console.log('Erro ao conectar', err.message);
+    }else{
+        console.log('Conectado com sucesso', err.message);
+    }
+})
+
+export default db;
